fc579abee01247f9f254344f95514a8e7073a2d7
Huy Vu <huy_htv@hotmail.com>
2025-02-06 11:06:05 +0100

    log querySelector innerHTML

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,24 @@
+/* 
+let myName = "Huy"
+let myAge = 28
+let myShoeSize = 40
+
+console.log('myName:', myName)
+console.log('myShoeSize:', myShoeSize)
+console.log('myAge:', myAge)
+
 const myH1 = document.querySelector("h1");
-console.log('myH1:', myH1);
+console.log('myH1:', myH1)
+myH1.style.background = "green";
+*/
 
-myH1.style.backgorund = "green";
+log("Mark");
+log("Winther");
+log("Hansen");
 
-
+function log(input){
+    // Fang body elementet
+const bodyEl = document.querySelector(".result");
+    // Sæt dens innerHTML til en tekst
+bodyEl.innerHTML += input + "<br>";
+}
